d0bc45960dd440d724caed59479da4d74971b910
jayatvitskiy <jayatvitskiy@ctemc.org>
2016-03-16 13:24:20 -0400

    The Passenger_WSGI file probably works
    Yay

--- passenger-wsgi.py
+++ passenger-wsgi.py
@@ -1,5 +1,28 @@
-import sys, os
-sys.path.append(os.path.join(os.getcwd(), 'projectname'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+# Detailed explanation at http://hitesh.in/2011/running-a-bottle-py-app-on-dreamhost/
+
+#1. Add current directory to path, if isn't already 
+import os, sys
+cmd_folder = os.path.dirname(os.path.abspath(__file__))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+import bottle
+from bottle import static_file,route,run,request
+
+#2. Define needed routes here	
+@route('/')
+def loadIndex():
+    return static_file('index.html', root='./') #sets up web page
+@route('/<filename:re:.*\..*>')#matches files
+def sendWebFile(filename):
+    return static_file(filename, root='./')
+@route('/runScript',method = "POST")
+def script():
+    execfile('comparetest.py')
+
+
+def application(environ, start_response):
+    return bottle.default_app().wsgi(environ,start_response)
+
+#4. Main method for local developement	
+if __name__ == "__main__":
+    run(host='localhost',port=8080,debug=True)
