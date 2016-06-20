4eb7a09ddbbeffa9b65436d18e83a24dc59cbfeb
jayatvitskiy <jayatvitskiy@ctemc.org>
2016-06-19 22:48:06 -0400

    minor change

--- comparetest.py
+++ comparetest.py
@@ -27,7 +27,7 @@ def compare(input_letter):
            return err
            
    #load saved uer image
-   user = cv2.imread("user.png",1)
+   user = cv2.imread("temp.png",1)
    user = cv2.cvtColor(user, cv2.COLOR_BGR2GRAY)
    rootdir = 'test'
             
