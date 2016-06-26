a7ab41af20039e96255901fbbe475e6ee09b65bb
jayatvitskiy <jayatvitskiy@ctemc.org>
2016-06-26 00:44:59 -0400

    comparetest server update

--- serverfiles/comparetest.py
+++ serverfiles/comparetest.py
@@ -27,13 +27,17 @@ def compare(input_letter):
            
    #load saved uer image
    user = cv2.imread("public/temp.png",1)
-
+   user = user[300:500,300:500,0:3]
+   cv2.imwrite('public/temp2.png',user)
    user = cv2.cvtColor(user, cv2.COLOR_BGR2GRAY)
    
    user = cv2.GaussianBlur(user,(35,35),0)
-   _, user = cv2.threshold(user,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-   
+   _, user = cv2.threshold(user,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+   user = Image.fromarray(user)
+   user.save('public/user.png')
+   #cv2.imshow('d',user)
+   user = cv2.imread("public/user.png")
+   user = cv2.cvtColor(user, cv2.COLOR_BGR2GRAY)
    rootdir = 'test'
    MSE={}
    #For each test file, add to the MSE dictionary
@@ -47,22 +51,22 @@ def compare(input_letter):
            MSE[file_name]=(mse(user, imageB))
    #Find range of MSE values and lowest 20% of MSE values
    MSE_range = max(MSE.values()) - min(MSE.values())
-   lowest_percent = MSE_range/2
+   lowest_percent = MSE_range/5
    #MSE value at 20th percentile
    lowest_percent_limit = min(MSE.values()) + (lowest_percent)
    match=False
    for file_name in MSE.keys():
-
         imageB = cv2.imread(file_name)
         imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-        
         #if MSE value between test image and user image 
         #falls between lowest 20%
         if file_name[12]==input_letter:
+        
            #if that test image corresponds with desired letter
+
            if min(MSE.values()) <= mse(user, imageB) <= lowest_percent_limit:
-           
                match=True
+   #print MSE
    return match
        #Rerun read and compare functions on same input letter
        #until the user gets sign correct
@@ -91,4 +95,3 @@ def learn(input_letter):
     while 1==1:
         read_letter(input_letter)
         compare(input_letter.upper())
-compare('C')
